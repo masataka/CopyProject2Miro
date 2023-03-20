6cfbdb7b96e0fcdaa80f1cc4a2a9d3dc277e8d7e
masataka <m@clipcrow.com>
2023-03-20 22:26:19 +0900

    fmt, link

--- README.md
+++ README.md
@@ -3,13 +3,16 @@
 ## Config
 
 Create /.env and write your Miro OAuth token like below.
+
 ```sh
-MIRO_TOKEN='your Miro OAuth token with boards:read, boards:write'
+MIRO_TOKEN='your Miro OAuth token with boards:read and boards:write'
+GITHUB_TOKEN='your GitHub personal access token with project'
 ```
 
 ## Run
 
 Run Deno app.
+
 ```sh
 % deno run --allow-env --allow-read --allow-net mod.ts
 ```
--- mod.ts
+++ mod.ts
@@ -2,32 +2,32 @@ import { load } from "https://deno.land/std@0.180.0/dotenv/mod.ts";
 const config = await load();
 
 async function main() {
-    const token = config.MIRO_TOKEN || "";
-    if (!token) {
-        throw new Error("Miro OAuth token not found.");
-    }
-    const response = await fetch("https://api.miro.com/v2/boards", {
+  const token = config.MIRO_TOKEN || "";
+  if (!token) {
+    throw new Error("Miro OAuth token not found.");
+  }
+  const response = await fetch("https://api.miro.com/v2/boards", {
     method: "POST",
     body: JSON.stringify({
-        name: "Hello World",
-        policy: {
-            permissionsPolicy: {
-            collaborationToolsStartAccess: "all_editors",
-            copyAccess: "team_editors",
-            sharingAccess: "owner_and_coowners"
+      name: "Hello World",
+      policy: {
+        permissionsPolicy: {
+          collaborationToolsStartAccess: "all_editors",
+          copyAccess: "team_editors",
+          sharingAccess: "owner_and_coowners",
         },
         sharingPolicy: {
-            access: "private",
-            inviteToAccountAndBoardLinkAccess: "no_access",
-            organizationAccess: "private",
-            teamAccess: "private"
-        }
+          access: "private",
+          inviteToAccountAndBoardLinkAccess: "no_access",
+          organizationAccess: "private",
+          teamAccess: "private",
         },
+      },
     }),
     headers: {
-        Authorization: `Bearer ${token}`,
-        accept: "application/json",
-        "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+      accept: "application/json",
+      "content-type": "application/json",
     },
   });
   return response;
